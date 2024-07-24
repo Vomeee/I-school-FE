8336f815133633ece201c95168dc0f8c285326c7
Vomeee <ye43019@gmail.com>
2024-07-24 10:40:06 +0900

    app, page 대응 파츠 추가

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import Page from 'app/page'
+import Courses from 'components/page/Courses'
+
+const CoursesPage = () => (
+  <Page title="CoursesPage">
+    <Courses/>
+  </Page>
+)
+
+export default CoursesPage
---
+++ src/components/page/Courses/index.tsx
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material"
+import 'styles/common.css'
+
+const Courses = () => {
+
+  return (
+    <Grid>
+      Hellllllo
+    </Grid>
+  )
+}
+
+export default Courses
