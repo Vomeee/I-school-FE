2e9ecd05bfbc32a202e57425b2dfc4c71cee39d1
Vomeee <ye43019@gmail.com>
2024-07-24 10:47:21 +0900

    검색창 대응 파츠 추가

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -2,9 +2,11 @@
 
 import Page from 'app/page'
 import Courses from 'components/page/Courses'
+import CourseSearcher from 'components/page/CourseSearcher'
 
 const CoursesPage = () => (
   <Page title="CoursesPage">
+    <CourseSearcher/>
     <Courses/>
   </Page>
 )
---
+++ src/components/page/CourseSearcher/index.tsx
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material"
+import 'styles/common.css'
+
+const CourseSearcher = () => {
+
+  return (
+    <Grid>
+      CourseSearcher
+    </Grid>
+  )
+}
+
+export default CourseSearcher
