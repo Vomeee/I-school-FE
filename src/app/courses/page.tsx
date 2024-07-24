053e9f262bbfaa5e9d889139a1ffe40d8499a004
Vomeee <ye43019@gmail.com>
2024-07-24 11:33:08 +0900

    css 확인, courses interface

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -3,11 +3,17 @@
 import Page from 'app/page'
 import Courses from 'components/page/Courses'
 import CourseSearcher from 'components/page/CourseSearcher'
+import 'styles/common.css'
+
 
 const CoursesPage = () => (
   <Page title="CoursesPage">
-    <CourseSearcher/>
-    <Courses/>
+    <div className='styles.container'>
+      <CourseSearcher/>
+      <Courses courseName={''} professor={''} rating={0}/>
+
+    </div>
+    
   </Page>
 )
 
